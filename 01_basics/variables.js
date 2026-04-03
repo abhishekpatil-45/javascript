699f00323bf066cab05c0b7694d3961a5e70f94f
abhishekpatil-45 <patilrohi40@gmail.com>
2026-04-03 10:25:56 +0000

    day2

---
+++ 01_basics/variables.js
@@ -0,0 +1,16 @@
+const accountID = 142637
+let accountEmail = "ranga@gmail.com" // methode to declare variable 
+var accountPassword = "367487" //methode to declare variable 
+accountCity = "mysore"//methode to declare variable 
+//accountID =2 // not allowed erorr beacuse const cant be changed
+accountEmail="dfkif@gmail.com"
+accountPassword = "4875"
+accountCity="nammuru"
+let accountState;//undefined 
+console.log(accountID)
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
+/*
+prefer not use to var
+because of functional scope and block scope 
+*/
